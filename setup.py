11bedbabd860e1a5628466ced844022e42c882ce
Luis C <lcajachahua@gmail.com>
2026-04-04 01:16:36 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import find_packages, setup  # Importando funciones de setuptools
-
 from typing import List                      # Importando el tipo Lista
 
 
-## Crea una función que abra requirment.txt y brinde una lista de pre-requisitos
+## Crea una función que abra requirements.txt y brinde una lista de pre-requisitos
 def get_requirments(file_path:str)->List[str]:
-    """ This function will return a list of all requirments """
+    """ This function will return a list of all requirements """
     
     requirments = []
     with open(file_path) as f:
@@ -26,6 +25,6 @@ setup(
     author = "Luis Caachahua",                              # Autor
     author_email = "lcajachahua@gmail.com",                 # Correo del Author
     packages = find_packages(),                             # Automáticamente encuentra librerías en el directorio actual
-    install_requires = get_requirments("requirements.txt")   # Especificar las dependencias en requirements.txt
+    install_requires = get_requirments("requirements.txt")  # Especificar las dependencias en requirements.txt
 
 )
